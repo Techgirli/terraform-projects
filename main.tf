fca25d2923f34eb1c8ed7bf2cf330e479aa39fa5
techgirli <olorunsolamiracle@gmail.com>
2025-12-16 10:05:05 -0500

    made some changes abd added a sensitive information

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "local_file" "tf_example1" {
   filename = "/home/techgirli/Downloads/terraform/01-basics/tf_example1.txt"
   content  = "Hey, this is the terraform course!. Miracle is a good learner."
 }
+
+resource "local_sensitive_file" "tf_example2" {
+  content  = "This is sensitive information!"
+  filename = "${path.module}/sensitive.txt"
+}
