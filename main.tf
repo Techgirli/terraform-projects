9a665841cd3042d1c7b9f3976582e72dd7119499
techgirli <olorunsolamiracle@gmail.com>
2025-12-28 18:25:23 -0500

    Added some changthe first linees on

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "local_file" "tf_example1" {
-  filename = "/home/techgirli/Downloads/terraform/01-basics/tf_example1.txt"
+  filename = "/home/techgirli/Downloads/terraform/01-basics/example-${count.index}.txt"
   content  = "Hey, this is the terraform course!. Miracle is a good learner."
   count    = 3
 }
