7b77427434e85a5334986168051027f3345792a9
techgirli <olorunsolamiracle@gmail.com>
2025-12-28 18:20:50 -0500

    Added count to create multiple files

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 resource "local_file" "tf_example1" {
   filename = "/home/techgirli/Downloads/terraform/01-basics/tf_example1.txt"
   content  = "Hey, this is the terraform course!. Miracle is a good learner."
+  count    = 3
 }
+
