d803f2e908123107657efa71db75a3460b3cef5d
techgirli <olorunsolamiracle@gmail.com>
2025-12-30 11:00:42 -0500

    new file:   vars-outputs/configs/DEV/server.sh

--- vars-outputs/mmain.tf
+++ vars-outputs/mmain.tf
@@ -9,40 +9,35 @@ terraform {
 provider "local" {
 }
 
-resource "local_file" "tf_example1" {
-  filename = "${path.module}/${var.filename-1}.demo"
-  content  = "Hey, this is the terraform course!. Miracle is a good learner."
-  count    = var.count_num
-}
-
-resource "local_file" "tf_example2" {
-  filename = "${path.module}/${var.filename-2}.txt"
-  content  = "Hey, this is the terraform course!. Miracle is a good learner."
-}
-
-resource "local_file" "tf_example3" {
-  filename = "${path.module}/${var.filename-3}.md"
-  content  = "Hey, this is the terraform course!. Miracle is a good learner."
-}
-
-locals {
-  base_path = "${path.module}/files"
-}
-resource "local_file" "tf_example4" {
-  filename = "${local.base_path}/tf_example.md"
-  content  = "Hey, this is the terraform course!. Miracle is a good learner."
-}
-
 locals {
   environment = "dev" # dev, staging, prod
-  upper_case  = upper(environment)
+  upper_case  = upper(local.environment)
   base_path   = "${path.module}/configs/${local.upper_case}"
 }
-
 resource "local_file" "service_configs" {
   filename = "${local.base_path}/server.sh"
   content  = <<EOT
   environment = ${local.environment}
   port=3000
   EOT
+}
+
+resource "local_file" "service_configs_2" {
+  filename = "${local.base_path}/server2.sh"
+  content  = <<EOT
+  environment = ${local.environment}
+  port=3000
+  EOT
+}
+resource "local_file" "service_configs_3" {
+  filename = "${local.base_path}/server3.sh"
+  content  = <<EOT
+  environment = ${local.environment}
+  port=3000
+  EOT
+}
+
+# outputs
+output "filename_1" {
+  value = local_file.service_configs.filename
 }
